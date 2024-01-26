bb6382b6bf9d5cf14e408c7bab19931c9bc23a92
averywh13 <awekslerhart@gmail.com>
2024-01-26 14:40:59 -0500

    added descriptions

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 function year1882() { 
-  
-  document.getElementById('response-text').innerText='Christmas tree, Oh Christmas tree! December 22, 1882 was the day that the first christmas tree was lit up by electrric lights.' 
+  document.getElementById('response-text').innerText='Christmas tree, Oh Christmas tree! December 22, 1882 was the day that the first christmas tree was lit up by electric lights in the home of Edward H. Johnson in NYC. He hand-wired 80 red, white, and blue electric light bulbs and decorated the Christmas tree. ' 
     document.getElementById('response-date').innerText= 'December 22, 1882' 
 } 
 
 function year1891() {
-
-  document.getElementById('response-text').innerText='first asteroid discovered through astrophotography'
+  document.getElementById('response-text').innerText='The first asteroid to be discovered using photography was 323 Brucia. It was discovered by the American astronomer Max Wolf on December 22, 1891, at the Heidelberg Observatory in Germany. Max Wolf pioneered the use of astrophotography, a technique that involves taking photographs of the night sky, to detect and study asteroids and other celestial objects. The discovery of 323 Brucia marked a significant advancement in observational astronomy during the late 19th century'
     document.getElementById('response-date').innerText= 'December 22, 1891'
 } 
 
 function year1978() {
 
-  document.getElementById('response-text').innerText='john wayne gacy confesses to his murders'
+  document.getElementById('response-text').innerText='John Wayne Gacy, the notorious American serial killer and sex offender, did not confess to his murders on December 22. Gacys arrest took place on December 21, 1978. Following his arrest, he initially denied any involvement in the crimes, but later, as the evidence against him became overwhelming, he eventually confessed to the murders'
     document.getElementById('response-date').innerText= 'December 22, 1978'
 }
 
 function year2001() {
 
-  document.getElementById('response-text').innerText='...'
+  document.getElementById('response-text').innerText='Richard C. Reid attempted to detonate a bomb hidden in his shoe on American Airlines Flight 63 from Paris to Miami. Fortunately, passengers and crew subdued him, preventing the attack. The incident led to increased security measures at airports worldwide'
     document.getElementById('response-date').innerText= 'December 22, 2001' 
 }
 
 function year2007() {
-
-  document.getElementById('response-text').innerText='john wayne gacy confesses to his murders'
+  document.getElementById('response-text').innerText='December 22, 2007 11:26am, me! I was born! '
     document.getElementById('response-date').innerText= 'December 22, 2007'
 }
