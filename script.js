cad4b4ee60c0abe1ac826164eec379763094962b
averywh13 <awekslerhart@gmail.com>
2024-01-26 21:04:43 -0500

    added pictures

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 function year1882() { 
-  document.getElementById('response-text').innerText='Christmas tree, Oh Christmas tree! December 22, 1882 was the day that the first christmas tree was lit up by electric lights in the home of Edward H. Johnson in NYC. He hand-wired 80 red, white, and blue electric light bulbs and decorated the Christmas tree. ' 
+  document.getElementById('changeImage').src = 'imgs/christmastree.png'
+  document.getElementById('response-text').innerText='Christmas tree, Oh Christmas tree! The first Christmas tree to be lit by electric lights was reportedly in the home of Edward H. Johnson, an associate of inventor Thomas Edison. This historic event took place on December 22, 1882, in New York City. Edward Johnson, who was an Edison employee, hand-wired 80 red, white, and blue electric light bulbs and decorated the Christmas tree in his home with them. This marked one of the early uses of electric lights for holiday decorations and contributed to the popularization of this practice in the years that followed. ' 
     document.getElementById('response-date').innerText= 'December 22, 1882' 
 } 
 
 function year1891() {
-  document.getElementById('response-text').innerText='The first asteroid to be discovered using photography was 323 Brucia. It was discovered by the American astronomer Max Wolf on December 22, 1891, at the Heidelberg Observatory in Germany. Max Wolf pioneered the use of astrophotography, a technique that involves taking photographs of the night sky, to detect and study asteroids and other celestial objects. The discovery of 323 Brucia marked a significant advancement in observational astronomy during the late 19th century'
+  document.getElementById('changeImage').src = 'imgs/asteroid.png'
+  document.getElementById('response-text').innerText='Quick! Make a wish! The first asteroid to be discovered using photography was 323 Brucia. It was discovered by the American astronomer Max Wolf on December 22, 1891, at the Heidelberg Observatory in Germany. Max Wolf pioneered the use of astrophotography, a technique that involves taking photographs of the night sky, to detect and study asteroids and other celestial objects. The discovery of 323 Brucia marked a significant advancement in observational astronomy during the late 19th century'
     document.getElementById('response-date').innerText= 'December 22, 1891'
 } 
 
 function year1978() {
-
-  document.getElementById('response-text').innerText='John Wayne Gacy, the notorious American serial killer and sex offender, did not confess to his murders on December 22. Gacys arrest took place on December 21, 1978. Following his arrest, he initially denied any involvement in the crimes, but later, as the evidence against him became overwhelming, he eventually confessed to the murders'
+  document.getElementById('changeImage').src = 'imgs/johnwaynegacy.webp'
+  document.getElementById('response-text').innerText='John Wayne Gacy confesses to police to killing over two dozen boys and young men and burying their bodies under his suburban Chicago home. In March 1980, Gacy was convicted of 33 sex-related murders, committed between 1972 and 1978, and later given the death penalty. '
     document.getElementById('response-date').innerText= 'December 22, 1978'
 }
 
 function year2001() {
-
-  document.getElementById('response-text').innerText='Richard C. Reid attempted to detonate a bomb hidden in his shoe on American Airlines Flight 63 from Paris to Miami. Fortunately, passengers and crew subdued him, preventing the attack. The incident led to increased security measures at airports worldwide'
+  document.getElementById('changeImage').src = 'imgs/richardcreid.jpg'
+  document.getElementById('response-text').innerText='Richard C. Reid boarded American Airlines Flight 63 from Paris to Miami with homemade bombs hidden in his shoes. During the flight, Reid tried to detonate his shoes, but struggled to light the fuse. Crew members and passengers noticed and restrained him. FBI agents determined that the shoes contained about ten ounces of explosive material. During a preliminary hearing. and FBI agent reveals how dangerous the bomb was. She said that the bomb techs determined that the bomb would have blown a hole in the planes fuselage and caused the plant to crash if it has detonated. '
     document.getElementById('response-date').innerText= 'December 22, 2001' 
 }
 
 function year2007() {
-  document.getElementById('response-text').innerText='December 22, 2007 11:26am, me! I was born! The blue-eyed, ginger-haired girl, that was born from two parents with dark hair and dark eyes. Strange...'
+  document.getElementById('changeImage').src = 'imgs/baby.PNG'
+  document.getElementById('response-text').innerText='December 22, 2007 11:26am, me! I was born! The blue-eyed, ginger-haired girl, that was born from two parents with dark hair and dark eyes. Hm, strange if you ask me. '
     document.getElementById('response-date').innerText= 'December 22, 2007'
 }
